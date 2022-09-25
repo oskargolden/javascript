116731ebf57369db780c093abd810694852fb8a9
Oskar Golden <oskar.golden@g.austincc.edu>
2022-09-25 12:17:29 -0500

    looked up anwser with the first part of problem with the foreach method.

--- credit_card_Validator.js
+++ credit_card_Validator.js
@@ -1,5 +1,3 @@
-//Codecademy credit card checker 
-
 // All valid credit card numbers
 const valid1 = [4, 5, 3, 9, 6, 7, 7, 9, 0, 8, 0, 1, 6, 8, 0, 8];
 const valid2 = [5, 5, 3, 5, 7, 6, 6, 7, 6, 8, 7, 5, 1, 4, 3, 9];
@@ -22,60 +20,66 @@ const mystery4 = [4, 9, 2, 9, 8, 7, 7, 1, 6, 9, 2, 1, 7, 0, 9, 3];
 const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3];
 
 // An array of all the arrays above
-const batch = [valid1, valid2, valid3, valid4, valid5, invalid1, invalid2, invalid3, invalid4, invalid5, mystery1, mystery2, mystery3, mystery4, mystery5];
-
+const batch = [
+  valid1,
+  valid2,
+  valid3,
+  valid4,
+  valid5,
+  invalid1,
+  invalid2,
+  invalid3,
+  invalid4,
+  invalid5,
+  mystery1,
+  mystery2,
+  mystery3,
+  mystery4,
+  mystery5,
+];
 
 // Add your functions below:
 //validate credit card function using Luhn Algo
-//iterate through the array of numbers, right to left, ie descending, probably using a for i--, as you itereate double EVERYOTHER the digit, 
-//if digit is greater than nine remove 9 from it. 
+//iterate through the array of numbers, right to left, ie decedning, probably using a for i--, as you itereate double the digit,
+//if digit is greater than nine remove 9 from it.
 //then sum up all digits. If modulo 10 = 0. return true. Else return false
-
-
 const validateCred = (arr) => {
-  let mainArr = arr
-  let test = []
-  let secondA = []
-  let arrLength = arr.length
-  // for(let i = arr.length; i >= 0; i--){
-  //   //forgot to double everyother digit. No wonder why it wasnt working.
-  //  //check indexof i in test array and if it remainder when divide by 2 == 0 then it needs to be *2
-  //  if(arr.indexOf(i) % 2 != 0){
-  //   test.push(arr[i])
-  //  } else{
-  //   test.push(arr[i] *2)
-  //  }
-    
-  // }
-  let reverse = mainArr.reverse()
-  console.log('arr: ',mainArr)
-  console.log('Reverse arr', reverse)
-  
-  for(let i = 0; i < arrLength; i++){
-    if(reverse.indexOf(i) % 2 != 0){
-      test.push(reverse[i])
-     } else{
-      test.push(reverse[i] *2)
-     }
+  cloneArr = arr;
+  cloneArr.reverse();
+  let result = [];
+  cloneArr.forEach((elem, idx) => {
+    if (idx % 2 === 0) {
+      result.push(elem);
+    } else {
+      if (elem * 2 > 9) {
+        result.push(elem * 2 - 9);
+      } else result.push(elem * 2);
+    }
+  });
+  let total = result.reduce((acu, cur) => acu + cur);
+  if (total % 10 === 0) {
+    return true;
+  } else {
+    return false;
   }
+};
 
-  
-  console.log('test arr: ',test)
-  
-  for(j = test.length; j >= 0; j--){
-   if(test[j] > 9){
-      secondA.push(test[j] - 9)
-   }else if (test[j] > 9){
-     secondA.push(test[j])
-   }
-  }
+console.log(validateCred(valid1));
 
-  console.log('secondA arr: ',secondA)
+//itteraing through nested array check to see if card is valid
+//if it is valid
+const findInvalidCards = (arr) => {
+  let batchArr = arr;
+  let badBatch = []
+  batchArr.forEach((elem) => {
+    if (validateCred(elem) === true) {
+      console.log("true");
+    } else if (validateCred(elem) === false) {
+      badBatch.push(elem)
+    }
+   
+  });
+   return badBatch
+};
 
- let sum = secondA.reduce((total, amount) => total + amount);
-
-
-  console.log(sum)
-}
-
-console.log(validateCred(valid5))
+findInvalidCards(batch)
